9a6b1428feec13dab365d07a5f65f5e416681dac
Ai Techno Crafts <ronitboy@yahoo.com>
2025-08-09 14:08:45 +0530

    Add files via upload

---
+++ Ai Technocrafts App Store/js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6YEgAXn8tIxdFPldZN1ap5ATi3BDOGq4",
+  authDomain: "ai-technocrafts-app-store.firebaseapp.com",
+  projectId: "ai-technocrafts-app-store",
+  storageBucket: "ai-technocrafts-app-store.firebasestorage.app",
+  messagingSenderId: "497852129252",
+  appId: "1:497852129252:web:3ce7c231ff1a00b2f5f1f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
